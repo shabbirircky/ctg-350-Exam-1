69173856f6b3f1a065e817ff5190eaafae57c160
shabbirricky. adnanhossain157@gmail.com <adnanhossain157@gmail.com>
2023-07-17 21:55:42 +0600

    first commit

---
+++ Exam-1/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+      lobster: ['Lobster Two', 'cursive'],
+      poppins: ['Poppins ', ' sans-serif'],
+    },
+    colors: {
+      'white': '#FFFFFF',
+      'text': '#707070',
+      'text-2': '#B3B3B3',
+      'orange': '#ff7849',
+      'sky': '#a5f3fc',
+      'yellow': '#fde68a',
+      'light-oeng': '#FF5959',
+      'pink': '#fecdd3',
+      'black': '#121212',
+    },
+  },
+
+  plugins: [],
+}
+
